9281ddb8b73b1f1439fe7d616886f896d0c3f2b8
Manara Ali <manaraali64@gmail.com>
2021-12-01 18:46:04 -0500

    Create App component

---
+++ jsx/src/App/index.jsx
@@ -0,0 +1,16 @@
+// 1. IMPORT REACT
+import React from "react";
+
+// 2. ADDITION IMPORTS
+
+// 3. CREATE A FUNCTION BASED COMPONENT
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+// 4. EXPORT COMPONENT TO BE USED IN OTHER PARTS OF OUR APPLICATION
+export default App;
---
+++ jsx/src/index.jsx
@@ -0,0 +1,11 @@
+// 1. IMPORT REACT
+import React from "react";
+
+// 2. IMPORT REACTDOM
+import ReactDOM from "react-dom";
+
+// 3. ADDITIONAL IMPORTS
+import App from "./App";
+
+// 4. RENDER COMPONENT TO THE SCREEN
+ReactDOM.render(<App />, document.querySelector("#root"));
